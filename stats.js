c88bcf477785c596eba1ccd2546c91b30e99435e
Simon Gavin <superbudvar@gmail.com>
2021-10-09 03:30:33 +0100

    fvnjklvgnjklfdnvk

---
+++ stats.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const filename = "./metadata.json";
+var tally = {};
+
+var res = require(filename);
+
+//console.log(res);
+res.forEach((element) => {
+  element.attributes.forEach((el) => {
+    if (tally[el.trait_type] == void 0) {
+      tally[el.trait_type] = {};
+    }
+    if (tally[el.trait_type][el.value] == void 0) {
+      tally[el.trait_type][el.value] = 0;
+    }
+    tally[el.trait_type][el.value]++;
+  });
+});
+
+for (let i in tally) {
+  let attrib = tally[i];
+  console.log("--");
+  console.log(i, ":");
+  // console.log(attrib);
+  let total = sum(attrib);
+  let multiplier = total / 100;
+  for (let v in attrib) {
+    let val = attrib[v];
+    //  console.log(v, val, multiplier);
+    console.log(`${v}: ${parseInt(attrib[v]) * multiplier}%`);
+  }
+}
+
+function sum(el) {
+  let sum = 0;
+  for (let i in el) {
+    sum += el[i];
+  }
+  return sum;
+}
